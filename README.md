94e1d7a50fc5d0ae8d7ea8015c9bb1c1e143f2b0
WhiteLuigi <151497404+WhiteLuigi@users.noreply.github.com>
2023-11-19 21:45:44 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# PsychEngine2
+# PsychEngineTwo
+
+WElcome to psych engine, TWO! the craziest engine out there with these mind blowing features that will kill you just READING them
+
+- No Mod Support
+- HTML5 Only Support
+- Built on the old ludem dare build
---
+++ source/Main.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		#if !desktop
+			addChild(new FlxGame(0, 0, TitleState));
+	
+			#if !mobile
+			addChild(new FPS(10, 3, 0xFFFFFF));
+			#end
+		#end
+	}
+}
